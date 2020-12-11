74b60b7bdcc77de72c1008e5069fbb6a8429df88
tony lombardi <alombardi.331@gmail.com>
2020-12-11 15:36:07 -0500

    update controller to accept slug

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -45,7 +45,7 @@ module Api
 
       # Use callbacks to share common setup or constraints between actions.
       def set_post
-        @post = Post.find(params[:id])
+        @post = Post.find_by_slug(params[:id])
       end
 
       # Only allow a trusted parameter "white list" through.
